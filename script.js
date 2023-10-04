3373a3776128362228693e72f62ee784ef1f6b25
Arrais22 <lucasarrais22@gmail.com>
2023-10-04 11:05:30 -0300

    Acrescentado operadores de comparação

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
-function generateNumber(){
+function generateNumber() {
 
     const min = Math.ceil(document.querySelector('.input-min').value)
     const max = Math.floor(document.querySelector('.input-max').value)
-    const result = Math.floor(Math.random() * (max - min + 1) + min);   
 
-    alert(result)
+
+    if (min >= max) {
+        alert("O valor mínimo deve ser MENOR que o valor Máximo")
+    } else {
+        const result = Math.floor(Math.random() * (max - min + 1) + min);
+        alert(result)
+    }   
 }
 
 const apereceresult = document.querySelector(".main-button")
 
-function resultado(){
+function resultado() {
     alert("result")
 }
 
-function id(){
+function id() {
     const p = document.querySelector(".Abufela")
     p.innerHTML = apareceresult.value
 }
